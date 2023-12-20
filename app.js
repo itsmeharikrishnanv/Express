e600a3b615ad75e7bac301e91eb9f7eb6c50d385
itsmeharikrishnanv <151928429+itsmeharikrishnanv@users.noreply.github.com>
2023-12-20 18:44:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 let http = require("http");
 
 let express = require('express');
+let bodyParser = require('body-parser');
 
 let app = express();
 
-app.use((req, res, next)=>{
-    console.log("Inside First middleware",req.originalUrl);
-    next();
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<html><head></head><body><form action="/product" method = "POST"><lebal for = "name">Name</lebal><input name = "title" type = "text" id = "name"><lebal for = "size">Size</lebal><input name = "size" type = "text" id = "size"><button type = "submit">Send</button></form></body></html>');
 })
 
-app.use((req,res,next)=>{
-    console.log("Inside Second middleware");
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello from Express.js!</h1>');
-    //res.end();
 })
 
 app.listen('4000');
